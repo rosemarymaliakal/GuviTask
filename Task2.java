251fc929e0781794f019aca76d175cac474aa90e
rosemarymaliakal <47358144+rosemarymaliakal@users.noreply.github.com>
2025-02-11 22:47:03 +0530

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,16 @@
+package GuviTask;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 252;
+		
+		int rem = num % 2;
+		
+		if(rem == 0) {
+			System.out.println("It's a even number");
+		}
+	}
+
+}
