c69a317fec613c20c727f92c4931cdbb239fc2f4
rosemarymaliakal <47358144+rosemarymaliakal@users.noreply.github.com>
2025-02-11 22:43:39 +0530

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,22 @@
+package GuviTask;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 100;
+		int b = 20;
+		int c = 30;
+		int d = 40;
+		
+		int sum1 = a + b;
+		int sum2 = c + d;
+		
+		if (sum1 > sum2) {
+			System.out.println("Sum of a and b is greater than sum of c and d");
+			
+		}
+
+	}
+
+}
