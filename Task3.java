3d9547b2b8669803d3e1c16a333c72ab8e58db11
rosemarymaliakal <47358144+rosemarymaliakal@users.noreply.github.com>
2025-02-11 22:53:37 +0530

    Add files via upload

---
+++ Task3.java
@@ -0,0 +1,14 @@
+package GuviTask;
+
+import java.util.Iterator;
+
+public class Task3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(char letter = 'A'; letter <= 'Z'; letter++) {
+			System.out.println(letter + " ");
+		}
+	}
+
+}
