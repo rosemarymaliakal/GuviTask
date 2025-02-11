e535cedaf6185b061405cddf9d6fca0cf15607e5
rosemarymaliakal <47358144+rosemarymaliakal@users.noreply.github.com>
2025-02-11 23:31:12 +0530

    Add files via upload

---
+++ Task10.java
@@ -0,0 +1,19 @@
+package GuviTask;
+import java.util.Scanner;
+
+public class Task10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter the number");
+		
+		String num = scanner.nextLine();
+		
+		int NumLength = num.length();
+
+		System.out.println("Length of number is "+NumLength);
+	}
+
+}
