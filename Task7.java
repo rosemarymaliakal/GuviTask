9d6b26d7b7974bca7890775d8e6593c4e782408e
rosemarymaliakal <47358144+rosemarymaliakal@users.noreply.github.com>
2025-02-11 23:18:32 +0530

    Add files via upload

---
+++ Task7.java
@@ -0,0 +1,15 @@
+package GuviTask;
+
+public class Task7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String Mymessage = "Guvi Geek";
+		
+		int LengthOfMeassage = Mymessage.length();
+		
+		System.out.println("Length of the message is "+LengthOfMeassage);
+
+	}
+
+}
