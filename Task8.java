204084b4ccf8dcc7916686fa265f83d094363738
rosemarymaliakal <47358144+rosemarymaliakal@users.noreply.github.com>
2025-02-11 23:24:20 +0530

    Add files via upload

---
+++ Task8.java
@@ -0,0 +1,15 @@
+package GuviTask;
+
+public class Task8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1;
+		while(a <= 10) {
+			System.out.println("Welcome to Guvi");
+			a = a + 1;
+		}
+
+	}
+
+}
